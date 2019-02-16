79c504ad8fad11e3ae98c8bd9c5015de447e4345
DeMesmaekerIndra <43603790+DeMesmaekerIndra@users.noreply.github.com>
2019-02-16 17:12:40 +0100

    Verder werken aan oef les 1
    HTML:
    -Toevoegen slider + label
    
    CSS:
    -weghalen imgwrapper div
    -opmaak slider
    
    JS:
    -Toevoegen eventlisteners voor buttons
    -Toevoegen window on load even listener (voorgestelde lay-out in de dia's)

--- Oefeningen/Les01/js/main.js
+++ Oefeningen/Les01/js/main.js
@@ -1,7 +1,24 @@
-(function() {
+(function () {
     'use strict';
 
-    let btnNormal = document.getElementById("btnNormal");
+    let van = document.getElementById('vanImg');
 
-   
+    window.addEventListener('load', function () {
+
+        document.getElementById('btnNormal').addEventListener('click', function () {
+            van.className = 'Normal';
+        });
+        document.getElementById('btnGrayscale').addEventListener('click', function () {
+            van.className = 'Grayscale';
+        });
+        document.getElementById('btnSepia').addEventListener('click', function () {
+            van.className = 'Sepia';
+        });
+        document.getElementById('btnHue').addEventListener('click', function () {
+            van.className = 'Hue';
+        });
+        document.getElementById('btnBlur').addEventListener('click', function () {
+            van.className = 'Blur';
+        });
+    })
 })()
