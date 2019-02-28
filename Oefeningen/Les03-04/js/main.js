4caea3d1ec5f5783eecfcbef576c8eea0c4005dd
DeMesmaekerIndra <43603790+DeMesmaekerIndra@users.noreply.github.com>
2019-02-28 12:54:13 +0100

    Kleine aanpassingen

--- Oefeningen/Les03-04/js/main.js
+++ Oefeningen/Les03-04/js/main.js
@@ -2,55 +2,63 @@
     'use strict';
 
     window.addEventListener('load', function () {
-        //Setting up the log in form
+        //LOGIN FORM
+
+        //Variables for the form
         let loginfrm = document.getElementById('login__form');
         let loginmodal = document.getElementById('loginmodal');
+        let inpEmail = document.getElementById('inpUname');               
+        let inpPassword = document.getElementById('inpPass');
+        let errEmail = document.getElementById('errUname');        
+        let errPassword = document.getElementById('errPass'); 
+
+        //Disable standard html form checking
         loginfrm.setAttribute('novalidate', 'novalidate');
 
-        //Eventhandler for login button
+        //Eventhandler for login button, show the loginmodal
         document.getElementById('btnLogin').addEventListener('click', function () {
             loginmodal.classList.remove('loginmodal__hidden');
             loginmodal.classList.add('loginmodal__shown');
         });
 
-        //Eventhandler for the cancel button on the form
+        //Eventhandler for the cancel button on the form. Hides the loginmodal and clears all errors, and input field
         document.getElementById('btnCancel').addEventListener('click', function () {
             loginmodal.classList.remove('loginmodal__shown');
             loginmodal.classList.add('loginmodal__hidden');
-        })
+            errEmail.innerHTML = '';
+            errPassword.innerHTML = '';
+            inpEmail.innerHTML = '';
+            inpPassword.innerHTML = '';
+        });
 
-        //Log in form checking
+        //login form input checking
         loginfrm.addEventListener('submit', function (e) {
             e.preventDefault();
             e.stopPropagation();
 
-            let inpEmail = document.getElementById('inpUname');
-            let inpPassword = document.getElementById('inpPass');
-            let errEmail = document.getElementById('errUname');
-            let errPassword = document.getElementById('errPass');
             let isValid = true;
 
             //Check username field
             if (inpEmail.value == '') {
                 errEmail.innerHTML = 'login kan niet leeg zijn';
-                isValid = false;
+                isValid = isValid && false;
             } else if (!inpEmail.value.includes('@')) {
                 errEmail.innerHTML = 'ongeldige login';
-                isValid = false;
+                isValid = isValid && false;
             } else {
                 errEmail.innerHTML = '';
-                isValid = true;
+                isValid = isValid && true;
             }
 
             //Check password field
-            if (inpPassword.value == null || inpPassword.value == '') {
-                errPassword.innerHTML = 'paswoord kan niet leeg zijn'
-                isValid = false;
+            if (inpPassword.value == '') {
+                errPassword.innerHTML = 'paswoord kan niet leeg zijn';
+                isValid = isValid && false;
             } else {
                 errPassword.innerHTML = '';
-                isValid = true;
+                isValid = isValid && true;
             }
-
+            
             //Final decision
             if (isValid) {
                 loginmodal.classList.remove('loginmodal__shown');
