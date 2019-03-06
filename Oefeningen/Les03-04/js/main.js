d5d24fa7ba889c44b5cefd573b61dbf55c3f5d90
DeMesmaekerIndra <43603790+DeMesmaekerIndra@users.noreply.github.com>
2019-03-06 17:35:35 +0100

    Grote aanpassingen aan filters, commentaar
    -Toevoegen commentaar
    -Toevoegen en gebruik maken van de resetFilters() en applyFilters() methodes
    
    -Toevoegen van de search bar filter
    
    -Alle filters maken nu gebruik van de methode applyFilters voor het nakijken van de conditie & het tonen of verbergen van de thumbnails

--- Oefeningen/Les03-04/js/main.js
+++ Oefeningen/Les03-04/js/main.js
@@ -1,6 +1,37 @@
 (function () {
     'use strict';
 
+    /**
+     * A method that allows us to check an attribute of an element.
+     * Thumbnails that didn't pass the condition will be hidden by adding a CSS class 'thumb__hidden'
+     * Thumbnails that did pass will have have the CSS class 'thumb__hidden' removed, and be shown.
+     * @param {*} conditionalValue Value you want to check against
+     * @param {string} attribute  The attribute name you want to get checked
+     * @param {string} selectors A string that defines the selectors of the items you want in collection that has to be checked
+     */
+    let applyFilters = function (conditionalValue, attribute, selectors) {
+        let thumbs = document.querySelectorAll('.main__thumbs>figure'); //Collection of thumbnails. This is needed to hide the entire thumbnail     
+        let wantedElements = document.querySelectorAll(selectors); //Collection where the lowest child element has to be checked for a certain condition
+
+        for (let i=0; i< wantedElements.length; i++) {
+            if (!wantedElements[i].getAttribute(attribute).toLowerCase().includes(conditionalValue.toLowerCase())) {
+                thumbs[i].classList.add('thumb__hidden'); //All of the thumbnails without a matching album-dataId will be hidden
+            } else {
+                thumbs[i].classList.remove('thumb__hidden'); //All matching thumbnails will be set as visible, could be hidden because of previous filter 
+            }
+        }
+    }
+
+    /**
+     * A method that removes the CSS class 'thumb__hidden' from all thumbnails, basically resetting every filter
+     */
+    let resetFilters = function () {
+        let thumbs = document.querySelectorAll('.main__thumbs>figure');
+        for (const thumb of thumbs) {
+            thumb.classList.remove('thumb__hidden');
+        }
+    }
+
     window.addEventListener('load', function () {
         //LOGIN FORM
 
@@ -104,47 +135,45 @@
         //Filter variables
         let dropDownFilter = document.getElementById('selAlbum');
         let checkboxFilters = document.querySelectorAll('.filters__years>label>input');
+        let btnFilter = document.getElementById('btnSearch');
+        let inpSearch = document.getElementById('inpSearch');
 
         //Filteren op basis van de drop down menu
         dropDownFilter.addEventListener('input', function () {
 
             if (dropDownFilter.value != -1) {
-                for (let thumb of thumbs) {
+                applyFilters(dropDownFilter.value, 'data-albumId', '.main__thumbs>figure');
 
-                    if (thumb.getAttribute('data-albumId') != dropDownFilter.value) {
-                        thumb.classList.add('thumb__hidden'); //All of the thumbnails without a matching album-dataId will be hidden
-                    } else {
-                        thumb.classList.remove('thumb__hidden'); //All matching thumbnails will be set as visible, could be hidden because of previous filter 
-                    }
-                }
             } else {
-                for (let thumb of thumbs) {
-                    thumb.classList.remove('thumb__hidden'); //Show all of the thumbnails if no filter hs been chosen
-                }
+                resetFilters(); //Show all of the thumbnails if no filter hs been chosen        
             }
         });
 
-        //Geef eventlisteners aan iedere checkbox filter toe
+        //Filtereren op basis van checkboxen
         for (let chkBox of checkboxFilters) {
             chkBox.addEventListener('input', function () {
                 //Toon alle thumbnails wanneer de checkbox niet geselecteerd is                           
                 if (!chkBox.checked) {
-                    for (let thumb of thumbs) {
-                        thumb.classList.remove('thumb__hidden');
-                    }
+                    resetFilters();
                     return; //Stoppen met het uitvoeren van de rest van de methode
                 }
 
                 dropDownFilter.value = -1; //Zet dropdown filter terug naar alle albums, we gaan niet per album per jaar filteren. De albums zijn al op jaar georderd.
-                for (let thumb of thumbs) {
-
-                    if (chkBox.value != thumb.getAttribute('data-year')) {
-                        thumb.classList.add('thumb__hidden');
-                    } else {
-                        thumb.classList.remove('thumb__hidden');
-                    }
-                }
+                applyFilters(chkBox.value, 'data-year', '.main__thumbs>figure');
             });
         }
+
+        btnFilter.addEventListener('click', function () {
+            if (inpSearch.value == '') {
+                resetFilters();
+                return;
+            }
+            for (const chk of checkboxFilters) {
+                chk.checked = false;
+            }
+            dropDownFilter.value = -1;
+
+            applyFilters(inpSearch.value, 'alt', '.main__thumbs>figure img');
+        });
     });
 })();
