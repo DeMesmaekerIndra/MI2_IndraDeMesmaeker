fb81467ff077869ff89d2c695444dfb2c9a4c994
DeMesmaekerIndra <43603790+DeMesmaekerIndra@users.noreply.github.com>
2019-03-09 18:20:19 +0100

    -Afwerken oefening
    -Navigatie knoppen
    -Navigatie met arrow keys
    -Slideshow
    -Toevoegen commentaar
    -Kleine aanpassingen

--- Oefeningen/Les03-04/js/main.js
+++ Oefeningen/Les03-04/js/main.js
@@ -2,7 +2,7 @@
     'use strict';
 
     /**
-     * A method that allows us to check an attribute of an element.
+     * A function that allows us to check an attribute of an element.
      * Thumbnails that didn't pass the condition will be hidden by adding a CSS class 'thumb__hidden'
      * Thumbnails that did pass will have have the CSS class 'thumb__hidden' removed, and be shown.
      * @param {*} conditionalValue Value you want to check against
@@ -12,18 +12,18 @@
     let applyFilters = function (conditionalValue, attribute, selectors) {
         let thumbs = document.querySelectorAll('.main__thumbs>figure'); //Collection of thumbnails. This is needed to hide the entire thumbnail     
         let wantedElements = document.querySelectorAll(selectors); //Collection where the lowest child element has to be checked for a certain condition
-        
+
         for (let i = 0; i < wantedElements.length; i++) {
             if (!wantedElements[i].getAttribute(attribute).toLowerCase().includes(conditionalValue.toLowerCase())) {
                 thumbs[i].classList.add('thumb__hidden');
             } else {
-                thumbs[i].classList.remove('thumb__hidden'); 
+                thumbs[i].classList.remove('thumb__hidden');
             }
         }
     }
 
     /**
-     * A method that removes the CSS class 'thumb__hidden' from all thumbnails, basically resetting every filter
+     * A function that removes the CSS class 'thumb__hidden' from all thumbnails, basicaly resetting every filter
      */
     let resetFilters = function () {
         let thumbs = document.querySelectorAll('.main__thumbs>figure');
@@ -33,17 +33,78 @@
     }
 
     /**
-     * A simple method that unselects all of the checkboxes 
+     * A function that will change the large image, desc, alt on the page with that of the selected thumbnail
+     * @param {Element} thumb The thumbnail (figure) that has to replace the large image
+     */
+    let changeLargeImage = function (thumb) {
+        let largeImg = document.querySelector('#large__figure>img')
+        let LargeImgDesc = document.getElementsByClassName('large__title')[0];
+        let activeThumb = document.getElementsByClassName('active')[0];
+        let img = thumb.querySelector('img');
+        let link = thumb.querySelector('a');
+
+        activeThumb.classList.remove('active');
+        thumb.classList.add('active');
+
+        largeImg.src = link.href;
+        LargeImgDesc.innerHTML = img.alt;
+        largeImg.alt = img.alt;
+    }
+
+    /**
+     * Function that unselects all of the checkboxes 
      */
     let resetChk = function () {
-       let checkboxFilters = document.querySelectorAll('.filters__years>label>input');
+        let checkboxFilters = document.querySelectorAll('.filters__years>label>input');
         for (const chk of checkboxFilters) {
             chk.checked = false;
         }
     }
 
+    /**
+     * Function that find the first previous thumbnail that has not been hidden
+     */
+    let previousThumb = function () {
+        let currentImg = document.getElementsByClassName('active')[0];
+
+        do {
+            currentImg = currentImg.previousElementSibling;
+            //If currentImg doesn't contain an element, stop executing the function. There are no previous element siblings left
+            if (currentImg == null) {
+                return;
+            }
+
+        } while (currentImg.classList.contains('thumb__hidden'));
+
+        changeLargeImage(currentImg);
+    }
+
+     /*intervalis where setInterval is stored for the slideshow, It's not defined in the window load event function
+    Because the function nextThumb() needs to acces it as well.*/
+    let interval = null;   
+
+    /**
+     * Function that finds the first next thumbnails that has not been hidden
+     */
+    let nextThumb = function () {
+        let currentImg = document.getElementsByClassName('active')[0];
+
+        do {
+            currentImg = currentImg.nextElementSibling;
+            //If currentImg doesn't contain an element, stop executing the function. There are no next element siblings left.
+            if (currentImg == null) {
+                clearInterval(interval);
+                interval = null;
+                return;
+            }
+        } while (currentImg.classList.contains('thumb__hidden'));
+        changeLargeImage(currentImg);
+    }    
+
     window.addEventListener('load', function () {
-        //LOGIN FORM
+        ///////////////
+        //LOGIN FORM//
+        //////////////
 
         //Form variables
         let loginfrm = document.getElementById('login__form');
@@ -110,43 +171,29 @@
             }
         });
 
-        //IMAGES
+        //////////
+        //IMAGES//
+        //////////
 
-        //Image variables
-        let largeImg = document.querySelector('#large__figure>img');
-        let LargeImgDesc = document.querySelector('.large__title');
-        let thumbs = document.querySelectorAll('.main__thumbs>figure');
-        let activeThumb;
+        //Image variables       
+        let thumbs = document.querySelectorAll('.main__thumbs>figure'); //A collection of all the thumbnails
 
-        //Find every small thumbnail, split up the link & img
-        //Add an eventlistener to every link
+        //Add eventlistener to every link from a thumbnail
         for (let thumb of thumbs) {
-            let link = thumb.querySelector('a');
-            let img = thumb.querySelector('img');
-
-            //change large image to selected small image, change alt & description
-            link.addEventListener('click', function (e) {
+            thumb.querySelector('a').addEventListener('click', function (e) {
                 e.preventDefault();
 
-                if (activeThumb != null) {
-                    activeThumb.classList.remove('active');
-                }
-                img.classList.add('active');
-                activeThumb = img;
-
-                largeImg.src = link.href;
-                LargeImgDesc.innerHTML = img.alt;
-                largeImg.alt = img.alt;
+                changeLargeImage(thumb);
             });
         }
 
-        //FILTERS
+        ///////////
+        //FILTERS//
+        ///////////
 
         //Filter variables
         let dropDownFilter = document.getElementById('selAlbum');
         let checkboxFilters = document.querySelectorAll('.filters__years>label>input');
-        let btnFilter = document.getElementById('btnSearch');
-        let btnReset = document.getElementById('btnReset');
         let inpSearch = document.getElementById('inpSearch');
 
         //Filter based on dropdown menu
@@ -173,7 +220,7 @@
                     return; //Stop executing the method
                 }
                 //Reset of all other filters
-                dropDownFilter.value = -1; 
+                dropDownFilter.value = -1;
                 inpSearch.value = '';
 
                 applyFilters(chkBox.value, 'data-year', '.main__thumbs>figure');
@@ -181,7 +228,7 @@
         }
 
         //Filter based on search field
-        btnFilter.addEventListener('click', function () {
+        document.getElementById('btnSearch').addEventListener('click', function () {
             //Show all thumbnails if the searchfield is empty
             if (inpSearch.value == '') {
                 resetFilters();
@@ -190,17 +237,77 @@
 
             //Reset of all other filters
             dropDownFilter.value = -1;
-            resetChk();          
+            resetChk();
 
             applyFilters(inpSearch.value, 'alt', '.main__thumbs>figure img');
         });
 
         //Reset all filters, show thumbnails
-        btnReset.addEventListener('click', function () {
+        document.getElementById('btnReset').addEventListener('click', function () {
             dropDownFilter.value = -1;
             inpSearch.value = '';
             resetChk();
             resetFilters();
+        });
+
+        /////////////
+        //NAVIGATIE//
+        /////////////
+
+        //Select the first image of the current set of shown images, and change the large image
+        document.getElementById('lnkFirst').addEventListener('click', function (e) {
+            e.preventDefault();
+
+            changeLargeImage(document.querySelectorAll('.main__thumbs>figure:not(.thumb__hidden)')[0])
+        });
+
+        //Select the last image of the current set of shown images, and change the large image
+        document.getElementById('lnkLast').addEventListener('click', function (e) {
+            e.preventDefault();
+
+            let shownThumbs = document.querySelectorAll('.main__thumbs>figure:not(.thumb__hidden)');
+            changeLargeImage(shownThumbs[shownThumbs.length - 1]);
+        });
+
+        //Show the previous not hidden thumbnail
+        document.getElementById('lnkPrev').addEventListener('click', function (e) {
+            e.preventDefault();
+            previousThumb();
+        });
+
+        //show the next thumbnail after clicking the next link
+        document.getElementById('lnkNext').addEventListener('click', function (e) {
+            e.preventDefault();
+            nextThumb();
+        });
+
+        //Show the first, previous, next, last thumbnails when an arrowkey (with/without shift) has been pressed
+        document.onkeydown = function (ev) {
+            let keyCode = ev.keyCode;
+            let shiftPressed = ev.shiftKey;
+
+            if (keyCode == 37 && !shiftPressed) {
+                previousThumb();
+            } else if (keyCode == 37 && shiftPressed) {
+                changeLargeImage(document.querySelectorAll('.main__thumbs>figure:not(.thumb__hidden)')[0])
+            }
+
+            if (keyCode == 39 && !shiftPressed) {
+                nextThumb();
+            } else if (keyCode == 39 && shiftPressed) {
+                let shownThumbs = document.querySelectorAll('.main__thumbs>figure:not(.thumb__hidden)');
+                changeLargeImage(shownThumbs[shownThumbs.length - 1]);
+            }
+        }
+        
+        document.getElementById('lnkPlay').addEventListener('click', function (e) {
+            e.preventDefault();
+            if (interval == null) {
+                interval = setInterval(nextThumb, 800);
+            } else {
+                clearInterval(interval);
+                interval = null;
+            }            
         });
     });
 })();
