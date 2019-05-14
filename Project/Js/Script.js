60901586aad7de333405cbc12f26c8dbf27b8499
DeMesmaekerIndra <43603790+DeMesmaekerIndra@users.noreply.github.com>
2019-05-14 10:55:51 +0200

    start building basic version minesweeper
    -HTML:
    Form asking for information
    table & tbody that'll be used as playing field
    
    -CSS:
    added style to form
    
    -JS:
    W.I.P.

--- Project/Js/Script.js
+++ Project/Js/Script.js
@@ -1,31 +1,10 @@
 (function () {
-    'use strict';
-    let video = document.getElementById('RawFootage'); // video is the id of video tag
-    let canvas = document.getElementById('playarea');
+
     window.addEventListener('load', function () {
-               
-        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-            .then(function(stream) {
-                video.srcObject = stream;
-                video.play();
-                setTimeout(processVideo, 0);
-            })
-            .catch(function(err) {
-                console.log("An error occurred! " + err);
-            });
-    });  
-    
-    function processVideo() {
-        let src = new cv.Mat(400, 400, cv.CV_8UC4);
-        let dst = new cv.Mat(400, 400, cv.CV_8UC1);
-        let cap = new cv.VideoCapture(video);
-        const FPS = 60;
-        let begin = Date.now();
-        cap.read(src);
-        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-        cv.imshow('playarea', dst);
-        // schedule next one.
-        let delay = 1000/FPS - (Date.now() - begin);
-        setTimeout(processVideo, delay);
-    }    
-})();   
+        let form = document.querySelector('form');
+
+        form.addEventListener('submit', function (e) {
+
+        });
+    });
+})();
