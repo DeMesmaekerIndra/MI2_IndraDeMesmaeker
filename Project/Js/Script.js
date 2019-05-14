d1918a27771fb1264c2bfa5f47e0b04f705e18d6
DeMesmaekerIndra <43603790+DeMesmaekerIndra@users.noreply.github.com>
2019-05-14 13:21:39 +0200

    Form checking
    -HTML:
    No notable changes
    
    -CSS:
    No notable changes
    
    -JS:
    Form checking

--- Project/Js/Script.js
+++ Project/Js/Script.js
@@ -1,43 +1,51 @@
 (function () {
 
+    let checkInput = function (input, errorElem) {
+        if (input.value.length === 0) {
+            errorElem.innerText = 'Please input a value';
+            errorElem.classList.remove('hidden', 'collapsed');
+            return false;
+        } else if (input.value.length < 3 && input.name === 'inpName') {
+            errorElem.innerText = 'Please use a username of atleast 3 characters';
+            errorElem.classList.remove('hidden', 'collapsed');
+            return false;
+        }
+
+        return true;
+    };
+
+    let createField = function(rows, columns){
+
+    };
+
     window.addEventListener('load', function () {
         let form = document.querySelector('form');
 
         form.addEventListener('submit', function (e) {
             e.preventDefault();
-            e.stopPropagation();
+            e.stopPropagation();            
+
+            let inpName = document.getElementById('inpName');
+            let inpRows = document.getElementById('inpRowSize');
+            let inpColumns = document.getElementById('inpColumnSize');
+
+            let errName = document.getElementById('errName');            
+            let errRows = document.getElementById('errRows');            
+            let errColumns = document.getElementById('errColumns');
 
             let isCorrect = true;
 
-            let inpName = document.getElementById('inpName');
-            let errName = document.getElementById('errName');
-            
-            errName.classList.add('hidden');
-            errName.classList.add('collapsed');
+            errName.classList.add('hidden', 'collapsed');
+            errRows.classList.add('hidden', 'collapsed');
+            errColumns.classList.add('hidden', 'collapsed');
 
-            if (inpName.value.length < 3) {
-                errName.classList.remove('hidden');
-                errName.classList.remove('collapsed');
+            isCorrect &= checkInput(inpName, errName);
+            isCorrect &= checkInput(inpRows, errRows);
+            isCorrect &= checkInput(inpColumns, errColumns);
 
-                errName.innerText = 'Please enter a name of at least 3 characters';
-                
-                isCorrect &= false;
+            if (isCorrect) {
+                createField(inpRows.value, inpColumns.value);
             }
-
-            let inpRows = document.getElementById('inpRowSize');
-            let errRows = document.getElementById('errRows');
-            
-            errRows.classList.add('hidden');
-            errRows.classList.add('collapsed');
-            console.log(inpRows.value);
-            
-            if (inpRows.value === '') {
-                errRows.classList.remove('hidden');
-                errRows.classList.remove('collapsed');
-
-                errRows.innerText = 'Please input a value'
-            }
-            
         });
-    }); 
+    });
 })();
