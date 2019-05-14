18c681e99a29ab42032496c9a0dc0c2b5fe3b8b1
DeMesmaekerIndra <43603790+DeMesmaekerIndra@users.noreply.github.com>
2019-05-14 12:01:46 +0200

    Form appearance & checking
    -HTML:
    moved inputs into the label elements
    added span elements for errors
    
    -CSS:
    general hidden & collapsed classes
    width changes, wrapping
    min & max widths to keep it responsive
    
    JS:
    Start of form checking

--- Project/Js/Script.js
+++ Project/Js/Script.js
@@ -4,7 +4,40 @@
         let form = document.querySelector('form');
 
         form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
 
+            let isCorrect = true;
+
+            let inpName = document.getElementById('inpName');
+            let errName = document.getElementById('errName');
+            
+            errName.classList.add('hidden');
+            errName.classList.add('collapsed');
+
+            if (inpName.value.length < 3) {
+                errName.classList.remove('hidden');
+                errName.classList.remove('collapsed');
+
+                errName.innerText = 'Please enter a name of at least 3 characters';
+                
+                isCorrect &= false;
+            }
+
+            let inpRows = document.getElementById('inpRowSize');
+            let errRows = document.getElementById('errRows');
+            
+            errRows.classList.add('hidden');
+            errRows.classList.add('collapsed');
+            console.log(inpRows.value);
+            
+            if (inpRows.value === '') {
+                errRows.classList.remove('hidden');
+                errRows.classList.remove('collapsed');
+
+                errRows.innerText = 'Please input a value'
+            }
+            
         });
-    });
+    }); 
 })();
